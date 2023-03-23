cf9feffcc5d5bc492414e49d1b2d89a8cb204850
Ward Fisher <wfisher@ucar.edu>
2023-03-23 11:57:12 -0600

    Updated invocation scripts to remove docker containers when they exit.

--- run_sync_docker_arm64.sh
+++ run_sync_docker_arm64.sh
@@ -44,4 +44,4 @@ echo "[Press any key to continue]"
 read -r
 
 
-docker run -d -h "${HNAME}" -p 8888:8888 -p 55555 -v "${TARGDIR}":/mnt/sync wardf/sync:arm64
+docker run -d --rm -h"${HNAME}" -p 8888:8888 -p 55555 -v "${TARGDIR}":/mnt/sync wardf/sync:arm64
